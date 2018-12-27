14e489a22b5cebf710905a792e23dea5ad2e0277
starryKey <1056838379@qq.com>
2018-12-27 23:49:07 +0800

    log:1、UDP

---
+++ 03-高级语法/19-net编程/UDPClient01.py
@@ -0,0 +1,31 @@
+"""
+- Client端流程
+    - 1、建立通信的socket
+    - 2、发送内容到指定的服务器
+    - 3、接受服务器给定的反馈内容
+
+"""
+
+import socket
+
+def clientFunc():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    text = "I miss you"
+
+    #发送的数据必须是bytes格式
+
+    data = text.encode()
+
+    # 发送
+    sock.sendto(data, ("127.0.0.1", 7582))
+
+    data, addr = sock.recvfrom(200)
+
+    data = data.decode()
+
+    print(data)
+
+if __name__ == '__main__':
+    clientFunc()
