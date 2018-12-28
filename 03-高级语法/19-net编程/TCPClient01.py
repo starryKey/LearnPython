9a678b0956ef3f6c8c540768521bd23bbc4de60b
starryKey <1056838379@qq.com>
2018-12-28 23:53:07 +0800

    log:1、fix tcp example error

--- 03-高级语法/19-net编程/TCPClient01.py
+++ 03-高级语法/19-net编程/TCPClient01.py
@@ -4,10 +4,11 @@ def tcp_client():
     #1、建立通信socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #2、链接对方，请求跟对方建立通路
-    addr = ("127.0.0.1", 8996)
+    addr = ("127.0.0.1", 8920)
     sock.connect(addr)
     #3、发送内容到对方服务器
-    msg = "T am testing"
+    msg = "I am testing"
+    sock.send(msg.encode())
     #4、接收对方的反馈
     rst = sock.recv(500)
     print(rst.decode())
@@ -16,4 +17,5 @@ def tcp_client():
     sock.close()
 
 if __name__ == '__main__':
+    print("client start connect....")
     tcp_client()
